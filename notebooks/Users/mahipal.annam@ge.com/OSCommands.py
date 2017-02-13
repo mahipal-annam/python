08057801710ca12231085f8432e4761e36be3435
mahipal-annam <mahipalannam@yahoo.com>
2017-02-13 11:11:40 -0800

    Checking in OS Commands Notebook

---
+++ notebooks/Users/mahipal.annam@ge.com/OSCommands.py
@@ -0,0 +1,38 @@
+# Databricks notebook source
+# MAGIC %sh date
+
+# COMMAND ----------
+
+# MAGIC %sh ls -l
+
+# COMMAND ----------
+
+# MAGIC %sh apt-get update
+
+# COMMAND ----------
+
+# MAGIC %sh date
+
+# COMMAND ----------
+
+# MAGIC %sh curl -v www.yahoo.com
+
+# COMMAND ----------
+
+# MAGIC %sh apt-get install awscli
+
+# COMMAND ----------
+
+# MAGIC %sh ping ec2-35-162-8-53.us-west-2.compute.amazonaws.com
+
+# COMMAND ----------
+
+# MAGIC %sh sudo apt-get update
+
+# COMMAND ----------
+
+# MAGIC %sql show tables
+
+# COMMAND ----------
+
+ 
